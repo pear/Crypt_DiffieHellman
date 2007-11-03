5598e2b91d11aff2075d3d19dd875ffac14a4fe8
Pádraic Brady <padraic@php.net>
2007-11-03 11:27:13 +0000

    renaming AllTests class to Crypt_DiffieHellman_AllTests
    git-svn-id: http://svn.php.net/repository/pear/packages/Crypt_DiffieHellman/trunk@245499 c90b9560-bf6c-de11-be94-00142212c4b1

--- tests/AllTests.php
+++ tests/AllTests.php
@@ -1,7 +1,7 @@
 <?php
 
 if (!defined('PHPUnit_MAIN_METHOD')) {
-    define('PHPUnit_MAIN_METHOD', 'AllTests::main');
+    define('PHPUnit_MAIN_METHOD', 'Crypt_DiffieHellman_AllTests::main');
 }
 
 require_once 'PHPUnit/Framework/TestSuite.php';
@@ -10,7 +10,7 @@ require_once 'PHPUnit/TextUI/TestRunner.php';
 require_once 'TestHelper.php';
 require_once 'DiffieHellmanTest.php';
 
-class AllTests
+class Crypt_DiffieHellman_AllTests
 {
     public static function main()
     {
@@ -32,6 +32,6 @@ class AllTests
     }
 }
 
-if (PHPUnit_MAIN_METHOD == 'AllTests::main') {
-    AllTests::main();
+if (PHPUnit_MAIN_METHOD == 'Crypt_DiffieHellman_AllTests::main') {
+    Crypt_DiffieHellman_AllTests::main();
 }
