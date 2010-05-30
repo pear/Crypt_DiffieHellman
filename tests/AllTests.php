435727a56ad45976245b453c7465b8bc8bd02162
Bill Shupp <shupp@php.net>
2010-05-30 03:44:59 +0000

    Tagging the 0.2.5 release
    git-svn-id: http://svn.php.net/repository/pear/packages/Crypt_DiffieHellman/tags/Crypt_DiffieHellman-0.2.5@299948 c90b9560-bf6c-de11-be94-00142212c4b1

---
+++ tests/AllTests.php
@@ -0,0 +1,36 @@
+<?php
+
+if (!defined('PHPUnit_MAIN_METHOD')) {
+    define('PHPUnit_MAIN_METHOD', 'Crypt_DiffieHellman_AllTests::main');
+}
+
+require_once 'PHPUnit/TextUI/TestRunner.php';
+
+require_once 'TestHelper.php';
+require_once 'DiffieHellmanTest.php';
+
+class Crypt_DiffieHellman_AllTests
+{
+    public static function main()
+    {
+        $parameters = array();
+
+        if (TESTS_GENERATE_REPORT && extension_loaded('xdebug')) {
+            $parameters['reportDirectory'] = TESTS_GENERATE_REPORT_TARGET;
+        }
+        PHPUnit_TextUI_TestRunner::run(self::suite(), $parameters);
+    }
+
+    public static function suite()
+    {
+        $suite = new PHPUnit_Framework_TestSuite('PEAR - Crypt_DiffieHellman');
+
+        $suite->addTestSuite('Crypt_DiffieHellmanTest');
+
+        return $suite;
+    }
+}
+
+if (PHPUnit_MAIN_METHOD == 'Crypt_DiffieHellman_AllTests::main') {
+    Crypt_DiffieHellman_AllTests::main();
+}
